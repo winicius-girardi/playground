b7cf7b6fd3aeec8ea4bf34b562513a065270d091
Winicius <wgirardi541@gmail.com>
2024-01-04 13:21:55 -0300

    Bonus v2

--- Bonus/RegisterEmployee.java
+++ Bonus/RegisterEmployee.java
@@ -1,22 +1,28 @@
 package Entrega.Compass.Bonus;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class RegisterEmployee {
     public static void main(String[] args){
+        createEmployee createEmployee= new createEmployee();
         Scanner scan=new Scanner(System.in);
+        int quantityEmployee;
         System.out.println("Quantos funcionários você quer cadastrar?");
-        int quantityEmployee=scan.nextInt();
-        String aux;
-        Double auxDouble;
+        try {
+            quantityEmployee=scan.nextInt();
+            if(quantityEmployee<=0)
+                throw new InputMismatchException();}
+        catch (InputMismatchException e) {
+            System.out.println("Quantidade de funcionários não pode ser negativa, tente novamente.");
+            quantityEmployee=scan.nextInt();
+        }
+        finally {
+            scan.nextLine();
+        }
         Employee[] employeeList = new Employee[quantityEmployee];
         for(int k=0;k<quantityEmployee;k++){
-            System.out.println("Nome do Funcionário:");
-            aux=scan.next();
-            System.out.println("Salário: ");
-            auxDouble= scan.nextDouble();
-            employeeList[k]= new Employee(aux,auxDouble);
-
+            createEmployee.scanEmployee(employeeList,k);
         }
         for(int k=0;k<quantityEmployee;k++){
             System.out.println(employeeList[k].toString());
